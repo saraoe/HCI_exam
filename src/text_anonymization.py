e9072370e63491c87e2c7b166096ea3c065a83d5
SaraMøllerØstergaard#1413 <sara.m.oestergaard@gmail.com>
2022-06-06 15:41:42 +0200

    multiple entities after each other

--- src/text_anonymization.py
+++ src/text_anonymization.py
@@ -27,14 +27,28 @@ def anon_text(text: str,
     doc = nlp(text)
     tokens = []
     for token in doc:
+        prev_token = tokens[-1] if len(tokens) != 0 else None
+        
         if token.text in non_entities:
             tokens.append(" " + token.text + " ")
         elif ((token.ent_type_ == "PERSON") or (token.text in selected_per)) & ("PER" in selected_entities):
-            tokens.append(("[NAME]", token.text, "#faa"))
+            if isinstance(prev_token, tuple) and prev_token[0] == "[NAME]":
+                tokens.pop(-1)
+                tokens.append(("[NAME]", prev_token[1]+' '+token.text, "#faa"))
+            else:
+                tokens.append(("[NAME]", token.text, "#faa"))
         elif ((token.ent_type_ in ["GPE", "LOC"]) or (token.text in selected_loc)) & ("LOC" in selected_entities):
-            tokens.append(("[LOCATION]", token.text, "#fda"))
+            if isinstance(prev_token, tuple) and prev_token[0] == "[LOCATION]":
+                tokens.pop(-1)
+                tokens.append(("[LOCATION]", prev_token[1]+' '+token.text, "#fda"))
+            else:
+                tokens.append(("[LOCATION]", token.text, "#fda"))
         elif ((token.ent_type_ == "ORG") or (token.text in selected_org)) & ("ORG" in selected_entities):
-            tokens.append(('[ORGANIZATION]', token.text, "#afa"))
+            if isinstance(prev_token, tuple) and prev_token[0] == "[ORGANIZATION]":
+                tokens.pop(-1)
+                tokens.append(("[ORGANIZATION]", prev_token[1]+' '+token.text, "#afa"))
+            else:
+                tokens.append(('[ORGANIZATION]', token.text, "#afa"))
         else:
             tokens.append(" " + token.text + " ")
 
